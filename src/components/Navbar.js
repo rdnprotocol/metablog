23d0b53c17440aca0f954698ced3154ee88fddad
24LP8224 24LP8224 <24LP8224@G301-CC0198.local>
2024-07-03 11:59:36 +0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between px-8 py-6">
+      <img className="h-[26px] w-[136px]" src="/Images/Logo.png" />
+      <img src="/Images/menu-outline.png" />
+    </div>
+  );
+};
